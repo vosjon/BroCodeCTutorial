a95d4bb42a477ae1be36e48b10a2f46596a48d68
Jonathan Vos <vosjonjoe@gmail.com>
2023-09-30 20:33:29 -0700

    Completed two lessons. The next lesson is data types

---
+++ CommentsEscape.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    // This is a comment. Blah.
+
+    /* 
+        This 
+        is 
+        a 
+        multi
+        line
+        comment
+    */
+
+   /* escape sequence = character combination consisting of a blackslash \ 
+                        followed by a letter or combination of digits.
+                        They specify actions within a line or string of text.
+                        \n = newline
+                        \t = tab
+    */
+
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9");
+    printf("\n\"I like pizza!\" - Abraham Lincoln probably");
+
+    return 0;
+}
